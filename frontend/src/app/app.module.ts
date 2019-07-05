766ddec316a2df8c47999d301973b50fa8094459
michal-lepik <michal.lepik@hotmail.com>
2019-07-05 16:17:23 +0200

    Basic mockup of main page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { PostTileComponent } from './components/post-tile/post-tile.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HomeComponent, HeaderComponent, PostTileComponent, SidebarComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ frontend/src/app/components/post-tile/post-tile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-post-tile',
+    templateUrl: './post-tile.component.html',
+    styleUrls: ['./post-tile.component.scss'],
+})
+export class PostTileComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
