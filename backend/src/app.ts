8eef1f275c2362f084db7c72a442b035adf932dc
michal-lepik <michal.lepik@hotmail.com>
2019-07-19 10:47:40 +0200

    Add tslint for backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,16 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
 
 const app = express();
 
+mongoose.connect(
+    `mongodb://mongo:27017/instapound_db`,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    },
+    // tslint:disable-next-line: no-console
+    () => console.log('mongo works'),
+);
+// tslint:disable-next-line: no-console
 app.listen(3000, () => console.log('server works'));
