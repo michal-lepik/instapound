8457a7bd963b6db889dd24f00a9519411f369881
michal-lepik <michal.lepik@hotmail.com>
2019-07-19 13:19:32 +0200

    Add env file and improve docker config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,15 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-mongoose.connect(
-    `mongodb://mongo:27017/instapound_db`,
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-    },
-    // tslint:disable-next-line: no-console
-    () => console.log('mongo works'),
-);
-// tslint:disable-next-line: no-console
-app.listen(3000, () => console.log('server works'));
+mongoose.connect(`process.env.DB_CONNECTION`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+app.listen(process.env.PORT);
